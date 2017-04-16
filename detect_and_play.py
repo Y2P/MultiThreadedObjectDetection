de555561acdb8f6ef7084e5c9f5a7fe78ab872c9
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-17 00:05:24 +0300

    final

--- detect_and_play.py
+++ detect_and_play.py
@@ -84,6 +84,7 @@ while True:
 	if(isinstance(ST.RedList, np.ndarray) and isinstance(ST.BlueList, list)):
 		print(ST.RedList[1])
 		distance2Line = -cv2.pointPolygonTest(ST.BlueList[0],(ST.RedList[0],ST.RedList[1]),True)
+		print(distance2Line)
 
 ## Distance to line from the barge point
 	try:
