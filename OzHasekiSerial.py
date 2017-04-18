c102704864a53c9f4ac1e856e1b7f77a5a72ca92
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-18 22:33:15 +0300

    commit

--- OzHasekiSerial.py
+++ OzHasekiSerial.py
@@ -1,13 +1,13 @@
 import serial
 def SendSerial(message):
     try:
-        ser = serial.Serial('/dev/ttyACM1',9600)
+        ser = serial.Serial('/dev/ttyUSB0',9600)
         ser.write(str(message))
     except:
         print('GGWP olduk')
 def ReadSerial():
     try:
-        ser = serial.Serial('/dev/ttyACM1',9600)
+        ser = serial.Serial('/dev/ttyUSB0',9600)
         message = ser.readline()
         return message
     except:
