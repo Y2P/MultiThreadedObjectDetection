cd8f2ddafdd5a94cbb15440344a0ef7bdb014d8b
Y2pi <yigit.pilavci01@gmail.com>
2017-04-19 23:12:11 +0300

    hey

--- OzHasekiSerial.py
+++ OzHasekiSerial.py
@@ -17,7 +17,7 @@ def ReadSerial():
         
 def ComLoop(SentMessage1,SentMessage2):
     ser = serial.Serial('/dev/ttyACM0',9600)
-	print("Message waiting")
-	mess = ser.readline()
-	print("Message is read",mess)
-	#ser.write(str(ST.distance2Line))
+    print("Message waiting")
+    mess = ser.readline()
+    print("Message is read",mess)
+    #ser.write(str(ST.distance2Line))
