cb9831aefc05525032a6b8a9c56ca00c6c72caa0
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-19 22:06:07 +0300

    hey

--- SetTrackBar.py
+++ SetTrackBar.py
@@ -17,12 +17,12 @@ def SetTrackBarsRed():
 	cv2.createTrackbar('Vlow','controller',0,255,nothing)
 	cv2.createTrackbar('Vhigh','controller',0,255,nothing)
 
-	cv2.setTrackbarPos('Hhigh','controller',179)
+	cv2.setTrackbarPos('Hhigh','controller',91)
 	cv2.setTrackbarPos('Shigh','controller',255)
 	cv2.setTrackbarPos('Vhigh','controller',255)
-	cv2.setTrackbarPos('Hlow','controller',98)
-	cv2.setTrackbarPos('Vlow','controller',65)
-	cv2.setTrackbarPos('Slow','controller',112)
+	cv2.setTrackbarPos('Hlow','controller',0)
+	cv2.setTrackbarPos('Vlow','controller',62)
+	cv2.setTrackbarPos('Slow','controller',93)
 
 
 def SetTrackBarsKernel():
@@ -48,9 +48,9 @@ def SetTrackBarsBlue():
 	cv2.createTrackbar('Vlow','controller-2',0,255,nothing)
 	cv2.createTrackbar('Vhigh','controller-2',0,255,nothing)
 
-	cv2.setTrackbarPos('Hhigh','controller-2',91)
+	cv2.setTrackbarPos('Hhigh','controller-2',179)
 	cv2.setTrackbarPos('Shigh','controller-2',255)
 	cv2.setTrackbarPos('Vhigh','controller-2',255)
-	cv2.setTrackbarPos('Hlow','controller-2',0)
-	cv2.setTrackbarPos('Vlow','controller-2',62)
-	cv2.setTrackbarPos('Slow','controller-2',93)
+	cv2.setTrackbarPos('Hlow','controller-2',98)
+	cv2.setTrackbarPos('Vlow','controller-2',65)
+	cv2.setTrackbarPos('Slow','controller-2',112)
