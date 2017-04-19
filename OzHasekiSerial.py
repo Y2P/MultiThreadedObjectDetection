755a307aead446e94555bd867cc3f00963649049
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-19 22:03:23 +0300

    hey

---
+++ OzHasekiSerial.py
@@ -0,0 +1,23 @@
+import serial
+import SetTrackBar as ST
+
+def SendSerial(message):
+    try:
+        ser = serial.Serial('/dev/ttyUSB0',9600)
+        ser.write(str(message))
+    except:
+        print('GGWP olduk')
+def ReadSerial():
+    try:
+        ser = serial.Serial('/dev/ttyUSB0',9600)
+        message = ser.readline()
+        return message
+    except:
+        print('GGWP olduk')
+        
+def ComLoop(SentMessage1,SentMessage2):
+    ser = serial.Serial('/dev/ttyACM0',9600)
+	print("Message waiting")
+	mess = ser.readline()
+	print("Message is read",mess)
+	#ser.write(str(ST.distance2Line))
