0fb3289a88c33f519f8608e1cd8e69b76828ab41
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-23 11:37:45 +0300

    hey

--- showResults.py
+++ showResults.py
@@ -9,6 +9,11 @@ cv2.namedWindow('thresholded')
 cv2.namedWindow('thresholded-2')
 cv2.namedWindow("temp")
 
+fourcc = cv2.cv.CV_FOURCC('H','2','6','1')
+out = cv2.VideoWriter()
+success = out.open('output.avi',fourcc,5.0,(352,288),1)   
+
 frame = 0
 def showRes(updatedframe,dummy):
-	cv2.imshow("temp",updatedframe)
+	cv2.imshow("temp",updatedframe)	
+	out.write(updatedframe)
